a6aec9c5bb377b89b4c565949700db47fc40ec1b
Xu Qingjian <sean.xu@dfgroup.pro>
2019-12-20 19:36:17 +0800

    add filter for ticks admin

--- market/admin.py
+++ market/admin.py
@@ -7,3 +7,9 @@ from market.models import Tick
 class TickAdmin(admin.ModelAdmin):
     list_display = ("__str__", "exchange", "symbol", "price", "amount", "value", "trade_time", "created",)
     list_editable = ("exchange", "symbol", "price", "amount", "value",)
+
+    list_filter = (
+        ("exchange", admin.RelatedOnlyFieldListFilter),
+        ("symbol", admin.RelatedOnlyFieldListFilter),
+    )
+
